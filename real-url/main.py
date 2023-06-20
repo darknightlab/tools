97294c7f80c1f4194671a11ca06af6afa88d1b07
canoziia <canoziia@qq.com>
2023-06-20 10:24:02 +0800

    initial

---
+++ real-url/main.py
@@ -0,0 +1,26 @@
+# 一个网页，接受get请求，从url参数中读取rid，并获取urlpath，传入自己的函数处理，这个函数返回一个网址，把用户302重定向到这个网址
+import flask
+from flask import redirect
+import importlib
+import json
+
+app = flask.Flask(__name__)
+
+
+@app.route("/<platform_name>/<rid>", methods=["GET"])
+def process_request(platform_name, rid):
+    redirect_url = process_rid(platform_name, rid)
+    return redirect(redirect_url, code=302)
+
+
+def process_rid(platform_name, rid):
+    # 导入 ./platform_name.py, 并执行get_real_url
+    p = importlib.import_module(platform_name)
+    return json.loads(p.get_real_url(rid))["m3u8"]
+
+
+if __name__ == "__main__":
+    port = 80
+    app.run(
+        port=port,
+    )
